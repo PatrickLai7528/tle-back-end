246819abda88daa0a53605bef61aae037476b9a7
Patrick Lai <patricklai7528@gmail.com>
2020-04-22 22:04:48 +0800

    udpate schema

--- app/model/Repository.ts
+++ app/model/Repository.ts
@@ -1,16 +1,47 @@
-
 export default app => {
    const mongoose = app.mongoose;
    const Schema = mongoose.Schema;
+
+   const TreeNodeSchema = new Schema({
+      _id: { type: Schema.Types.ObjectId, auto: true },
+      sha: { type: String },
+      path: { type: String },
+      fullyQuailafiedName: { type: String },
+      type: { type: String, enum: ["FOLDER", "FILE"] },
+   })
+
+   TreeNodeSchema.add({ subTrees: [TreeNodeSchema] })
 
    const RepositorySchema = new Schema({
       _id: { type: Schema.Types.ObjectId, auto: true },
       name: { type: Schema.Types.String, required: true },
       ownerId: { type: Schema.Types.String },
       currentBranch: { type: Schema.Types.String },
-      branches: { type: Schema.Types.Mixed },
-      trees: { type: Schema.Types.Mixed },
-      commits: { type: Schema.Types.Mixed },
+      branches: [
+         {
+            _id: { type: Schema.Types.ObjectId, auto: true },
+            name: { type: String, required: true },
+         }
+      ],
+      trees: [TreeNodeSchema],
+      commits: [
+         {
+            _id: { type: Schema.Types.ObjectId, auto: true },
+            sha: { type: String },
+            changedFiles: [
+               {
+                  _id: { type: Schema.Types.Objected, auto: true },
+                  sha: { type: String },
+                  filename: { type: String },
+                  status: { type: String },
+                  additions: { type: Number },
+                  deletions: { type: Number },
+                  path: { type: String },
+                  rawContent: { type: String }
+               }
+            ]
+         }
+      ],
       language: { type: Schema.Types.String },
       description: { type: Schema.Types.String },
       lastUpdateAt: { type: Schema.Types.Number },
