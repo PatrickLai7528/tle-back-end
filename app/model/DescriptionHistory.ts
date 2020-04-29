147b95b72dc91c12dd917f8e2f4034c573c5bfee
Patrick Lai <patricklai7528@gmail.com>
2020-04-29 21:51:46 +0800

    refactor: description history schema

--- app/model/DescriptionHistory.ts
+++ app/model/DescriptionHistory.ts
@@ -7,7 +7,10 @@ export default (app) => {
       _id: { type: Schema.Types.ObjectId, auto: true },
       ownerId: { type: Schema.Types.String, required: true },
       requirement: { type: Schema.Types.ObjectId, ref: "Requirement" },
-      description: { type: Schema.Types.String, ref: "RequirementDescription" },
+      description: {
+        type: Schema.Types.ObjectId,
+        ref: "RequirementDescription",
+      },
       oldDescription: { type: Schema.Types.Mixed },
       newDescription: { type: Schema.Types.Mixed },
       createAt: { type: Number, default: Date.now() },
