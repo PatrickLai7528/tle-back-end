b8abdfeb7cc00a0856774402dd45552aefc7f480
Patrick Lai <patricklai7528@gmail.com>
2020-04-29 14:58:47 +0800

    chore: add mock data

---
+++ app/mock/Requirement.mock.ts
@@ -0,0 +1,13 @@
+import { IRequirement } from "../entity/types";
+import { requirementDescriptionMocks } from "./RequirementDescription";
+
+const fromIndex = Math.round(
+  Math.random() * requirementDescriptionMocks.length
+);
+
+export const requirementMock: IRequirement = {
+  _id: "jkladjf",
+  relatedRepoName: "RELATED REPO NAME",
+  relatedRepoOwnerId: "RELATED REPO OWNER ID",
+  descriptions: requirementDescriptionMocks.slice(fromIndex, fromIndex + 10),
+};
