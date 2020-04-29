a01610bbe01e2524ce1c1cc4bfcae79d539c6866
Patrick Lai <patricklai7528@gmail.com>
2020-04-29 22:06:00 +0800

    refactor: user schema timestamps

--- app/model/User.ts
+++ app/model/User.ts
@@ -1,23 +1,24 @@
 import { Schema } from "mongoose";
-export default app => {
-   const mongoose = app.mongoose;
-   const Schema = mongoose.Schema;
+export default (app) => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
 
-   const UserSchema: Schema = new Schema(
-      {
-         _id: { type: Schema.Types.ObjectId, auto: true },
-         email: { type: String, required: true, unique: true },
-         password: { type: String, required: true },
-         githubId: { type: String, default: null },
-         createAt: { type: Number, default: Date.now() },
-         lastUpdateAt: { type: Number, default: Date.now() },
+  const UserSchema: Schema = new Schema(
+    {
+      _id: { type: Schema.Types.ObjectId, auto: true },
+      email: { type: String, required: true, unique: true },
+      password: { type: String, required: true },
+      githubId: { type: String, default: null },
+      createAt: { type: Number, default: Date.now() },
+      lastUpdateAt: { type: Number, default: Date.now() },
+    },
+    {
+      timestamps: {
+        createdAt: "createAt",
+        updatedAt: "lastUpdateAt",
       },
-      {
-         timestamps: {
-            createAt: "createAt",
-            updateAt: "lastUpdateAt"
-         },
-      });
+    }
+  );
 
-   return mongoose.model('User', UserSchema, 'User');
+  return mongoose.model("User", UserSchema, "User");
 };
